f9bb976a219e2aabb59074a9c338acf35feb546a
srio <srio@esrf.eu>
2021-12-06 11:17:17 +0100

    first commit

---
+++ examples/demo_pyvinyl_shadow3.py
@@ -0,0 +1,83 @@
+from libpyvinyl.BaseCalculator import CalculatorParameters as Parameters
+
+
+import sys
+
+
+from shadow3libpyvinyl.Shadow3Calculator import Shadow3Calculator
+
+
+calculator  = Shadow3Calculator("")
+
+### Setup the parameters
+calculator.setParams(number_of_optical_elements=1)
+
+print(">>>>>DEFAULTS: ", calculator.parameters, "\n\n\n")
+
+calculator.parameters["oe0.FDISTR"].value = 3
+calculator.parameters["oe0.F_COLOR"].value = 3
+calculator.parameters["oe0.F_PHOT"].value = 0
+calculator.parameters["oe0.HDIV1"].value = 0.0
+calculator.parameters["oe0.HDIV2"].value = 0.0
+calculator.parameters["oe0.NPOINT"].value = 10000
+calculator.parameters["oe0.PH1"].value = 8799.999
+calculator.parameters["oe0.PH2"].value = 8799.999
+calculator.parameters["oe0.SIGDIX"].value = 4.728541797631135e-06
+calculator.parameters["oe0.SIGDIZ"].value = 4.095010077148124e-06
+calculator.parameters["oe0.SIGMAX"].value = 0.0015810951361940363
+calculator.parameters["oe0.SIGMAZ"].value = 0.0006681031579752021
+calculator.parameters["oe0.VDIV1"].value = 0.0
+calculator.parameters["oe0.VDIV2"].value = 0.0
+
+calculator.parameters["oe1.DUMMY"].value = 1.0
+calculator.parameters["oe1.FMIRR"].value = 3
+calculator.parameters["oe1.FWRITE"].value = 1
+calculator.parameters["oe1.T_IMAGE"].value = 1000.0
+calculator.parameters["oe1.T_INCIDENCE"].value = 89.828
+calculator.parameters["oe1.T_REFLECTION"].value = 89.828
+calculator.parameters["oe1.T_SOURCE"].value = 4000.0
+
+
+### Run the backengine
+calculator.backengine()
+
+
+import Shadow
+from srxraylib.plot.gol import set_qt
+set_qt()
+Shadow.ShadowTools.plotxy(calculator.data, 1, 3, nbins=101, nolost=1, title="Real space")
+
+### Look at the data and store as hdf5
+print(calculator.data)
+calculator.saveH5("tmp.h5")
+
+
+
+if True: # errors here!!!!
+
+    ### Save the parameters to a human readable json file.
+    print(calculator.parameters["oe1.CCC"])
+    calculator.parameters.to_json("my_parameters.json")
+
+    ### Save calculator to binary dump.
+    # dumpfile = calculator.dump()
+
+    ########################################################################################################################
+
+    ### Load back parameters
+
+    new_parameters = Parameters.from_json("my_parameters.json")
+
+    new_parameters["oe0.NPOINT"] = 10000
+
+    print('oe0.NPOINT:', new_parameters['oe0.NPOINT'])
+
+    new_calculator  = Shadow3Calculator("", output_path="out.h5")
+
+    new_calculator.setParams(native=new_parameters)
+
+    new_calculator.backengine()
+
+    new_calculator.saveH5("tmp.h5")
+
+
---
+++ shadow3libpyvinyl/Shadow3Calculator.py
@@ -0,0 +1,149 @@
+from libpyvinyl.BaseCalculator import BaseCalculator, CalculatorParameters
+import Shadow
+import inspect
+
+class Shadow3Calculator(BaseCalculator):
+    def __init__(self,
+                 name,
+                 parameters=None,
+                 dumpfile=None,
+                 input_path=None,
+                 output_path=None):
+
+        super().__init__(name,
+                         parameters=parameters,
+                         dumpfile=dumpfile,
+                         output_path=output_path)
+
+        self.number_of_optical_elements = 0
+
+    def setParams(self,
+            source=None,
+            beamline=[],
+            number_of_optical_elements = 0,
+            native=None,
+            ):
+
+        # native format from json
+        if native is not None:
+            self.parameters = native
+            return
+
+        # source
+        if isinstance(source, Shadow.Source):
+            oe0 = source
+        else:
+            oe0 = Shadow.Source()
+
+        oe0_dict = oe0.to_dictionary()
+
+        if not isinstance(self.parameters, CalculatorParameters):
+            self.parameters = CalculatorParameters()
+
+        for key in oe0_dict.keys():
+            self.parameters.new_parameter("oe0."+key)
+            if isinstance(oe0_dict[key], bytes):
+                self.parameters["oe0."+key].set_value(oe0_dict[key].decode("utf-8"))
+            else:
+                self.parameters["oe0."+key].set_value(oe0_dict[key])
+
+        # beamline
+        if len(beamline) == 0 and number_of_optical_elements > 0:
+            beamline = [Shadow.OE()] * number_of_optical_elements
+
+        for i in range(len(beamline)):
+            if isinstance(beamline[i], Shadow.OE):
+                oe_i = beamline[i]
+            else:
+                oe_i = Shadow.OE()
+
+            oe_i_dict = oe_i.to_dictionary()
+
+            if not isinstance(self.parameters, CalculatorParameters):
+                self.parameters = CalculatorParameters()
+
+            for key in oe_i_dict.keys():
+                self.parameters.new_parameter("oe%d.%s" % (i+1, key))
+                if isinstance(oe_i_dict[key], bytes):
+                    self.parameters["oe%d.%s" % (i+1, key)].set_value(oe_i_dict[key].decode("utf-8"))
+                else:
+                    self.parameters["oe%d.%s" % (i+1, key)].set_value(oe_i_dict[key])
+
+        self.number_of_optical_elements = number_of_optical_elements
+
+    def __get_valiable_list(self, object1):
+        """
+        returns a list of the Shadow.Source or Shadow.OE variables
+        """
+        mem = inspect.getmembers(object1)
+        mylist = []
+        for i,var in enumerate(mem):
+            if var[0].isupper():
+                mylist.append(var[0])
+        return(mylist)
+
+    def backengine(self):
+        beam = Shadow.Beam()
+        oe0 = Shadow.Source()
+
+
+        oe0_list = self.__get_valiable_list(oe0)
+
+        for name in oe0_list:
+            try:
+                value = self.parameters["oe0."+name].value
+                if isinstance(value, str):
+                    value = bytes(value, 'UTF-8')
+                setattr(oe0, name, value)
+            except:
+                raise Exception("Error setting parameters name %s" % name)
+
+
+        beam.genSource(oe0)
+
+        if self.number_of_optical_elements > 0:
+            oei_list = self.__get_valiable_list(Shadow.OE())
+            for i in range(self.number_of_optical_elements):
+                oe_i = Shadow.OE()
+                for name in oei_list:
+                    try:
+                        value = self.parameters["oe%d.%s" % (i+1, name)].value
+                        if isinstance(value, str):
+                            value = bytes(value, 'UTF-8')
+                        setattr(oe_i, name, value)
+                    except:
+                        raise Exception("Error setting parameters name %s" % name)
+
+                beam.traceOE(oe_i, i+1)
+
+        self._set_data(beam)
+        return 0
+
+
+    def dump(self,filename="star.01"): # overwritten method
+        self.data.write(filename)
+
+    def saveH5(self, filename="tmp.h5", openpmd=False):
+        from orangecontrib.panosc.shadow.util.openPMD import saveShadowToHDF
+        saveShadowToHDF(self.data, filename=filename)
+
+
+
+if __name__ == "__main__":
+    calculator = Shadow3Calculator("")
+
+    ### Setup the parameters
+    calculator.setParams(number_of_optical_elements=1)
+
+
+    ### Run the backengine
+    calculator.backengine()
+
+    ### Plot results using ShadowTools
+    try:
+        from srxraylib.plot.gol import set_qt
+        set_qt()
+    except:
+        pass
+
+    Shadow.ShadowTools.plotxy(calculator.data, 1, 3, nbins=101, nolost=1, title="Real space")
