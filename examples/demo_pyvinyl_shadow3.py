6f9074e32fe09318ac81f6f962f9bac36a679e7e
Manuel Sanchez del Rio <srio@esrf.eu>
2022-09-01 12:45:03 +0200

    update

--- examples/demo_pyvinyl_shadow3.py
+++ examples/demo_pyvinyl_shadow3.py
@@ -1,44 +1,46 @@
 
 
 from shadow3libpyvinyl.Shadow3Calculator import Shadow3Calculator
+from libpyvinyl.Parameters.Collections import CalculatorParameters
+from libpyvinyl.Parameters.Parameter import Parameter
 
+########################################################################################################################
+#
+# auxiliary functions
+#
 
-if True:
-    calculator  = Shadow3Calculator("")
+def get_calculator_parameters(source=True,oe=True):
 
-    calculator.setParams(number_of_optical_elements=1)
+    parameters = CalculatorParameters()
 
+    if source:
+        p = Parameter("oe0.FDISTR"      , "" )  ; p.value   = 3                        ; parameters.add(p)
+        p = Parameter("oe0.F_COLOR"     , "" )  ; p.value   = 3                        ; parameters.add(p)
+        p = Parameter("oe0.F_PHOT"      , "" )  ; p.value   = 0                        ; parameters.add(p)
+        p = Parameter("oe0.HDIV1"       , "" )  ; p.value   = 0.0                      ; parameters.add(p)
+        p = Parameter("oe0.HDIV2"       , "" )  ; p.value   = 0.0                      ; parameters.add(p)
+        p = Parameter("oe0.NPOINT"      , "" )  ; p.value   = 10000                    ; parameters.add(p)
+        p = Parameter("oe0.PH1"         , "" )  ; p.value   = 8799.999                 ; parameters.add(p)
+        p = Parameter("oe0.PH2"         , "" )  ; p.value   = 8799.999                 ; parameters.add(p)
+        p = Parameter("oe0.SIGDIX"      , "" )  ; p.value   = 4.728541797631135e-06    ; parameters.add(p)
+        p = Parameter("oe0.SIGDIZ"      , "" )  ; p.value   = 4.095010077148124e-06    ; parameters.add(p)
+        p = Parameter("oe0.SIGMAX"      , "" )  ; p.value   = 0.0015810951361940363    ; parameters.add(p)
+        p = Parameter("oe0.SIGMAZ"      , "" )  ; p.value   = 0.0006681031579752021    ; parameters.add(p)
+        p = Parameter("oe0.VDIV1"       , "" )  ; p.value   = 0.0                      ; parameters.add(p)
+        p = Parameter("oe0.VDIV2"       , "" )  ; p.value   = 0.0                      ; parameters.add(p)
 
-    calculator.parameters["oe0.FDISTR"]       = 3
-    calculator.parameters["oe0.F_COLOR"]      = 3
-    calculator.parameters["oe0.F_PHOT"]       = 0
-    calculator.parameters["oe0.HDIV1"]        = 0.0
-    calculator.parameters["oe0.HDIV2"]        = 0.0
-    calculator.parameters["oe0.NPOINT"]       = 10000
-    calculator.parameters["oe0.PH1"]          = 8799.999
-    calculator.parameters["oe0.PH2"]          = 8799.999
-    calculator.parameters["oe0.SIGDIX"]       = 4.728541797631135e-06
-    calculator.parameters["oe0.SIGDIZ"]       = 4.095010077148124e-06
-    calculator.parameters["oe0.SIGMAX"]       = 0.0015810951361940363
-    calculator.parameters["oe0.SIGMAZ"]       = 0.0006681031579752021
-    calculator.parameters["oe0.VDIV1"]        = 0.0
-    calculator.parameters["oe0.VDIV2"]        = 0.0
-    calculator.parameters["oe1.DUMMY"]        = 1.0
-    calculator.parameters["oe1.FMIRR"]        = 3
-    calculator.parameters["oe1.FWRITE"]       = 1
-    calculator.parameters["oe1.T_IMAGE"]      = 1000.0
-    calculator.parameters["oe1.T_INCIDENCE"]  = 89.828
-    calculator.parameters["oe1.T_REFLECTION"] = 89.828
-    calculator.parameters["oe1.T_SOURCE"]     = 4000.0
+    if oe:
+        p = Parameter("oe1.DUMMY"       , "" )  ; p.value   = 1.0                      ; parameters.add(p)
+        p = Parameter("oe1.FMIRR"       , "" )  ; p.value   = 3                        ; parameters.add(p)
+        p = Parameter("oe1.FWRITE"      , "" )  ; p.value   = 1                        ; parameters.add(p)
+        p = Parameter("oe1.T_IMAGE"     , "" )  ; p.value   = 1000.0                   ; parameters.add(p)
+        p = Parameter("oe1.T_INCIDENCE" , "" )  ; p.value   = 89.828                   ; parameters.add(p)
+        p = Parameter("oe1.T_REFLECTION", "" )  ; p.value   = 89.828                   ; parameters.add(p)
+        p = Parameter("oe1.T_SOURCE"    , "" )  ; p.value   = 4000.0                   ; parameters.add(p)
 
+    return parameters
 
-    ### Run the backengine
-    calculator.backengine(write_start_files_root="start")
-
-
-    #
-    # make plot
-    #
+def do_plotxy_plot(calculator, title=""):
     import Shadow
     try:
         from srxraylib.plot.gol import set_qt
@@ -46,28 +48,68 @@ if True:
     except:
         pass
 
-    Shadow.ShadowTools.plotxy(calculator.data, 1, 3, nbins=101, nolost=1, title="Real space")
+    beam = Shadow.Beam(N=calculator.data.get_data()["nrays"])
+    beam.rays = calculator.data.get_data()["rays"]
+    Shadow.ShadowTools.plotxy(beam, 1, 3, nbins=101, nolost=1, title=title+" Real space")
 
 
-    #
-    # save files
-    calculator.saveH5("tmp.h5")
-    calculator.parameters.to_json("my_parameters.json")
-    print(calculator.parameters)
+########################################################################################################################
+
 
 #
-# retrieve from json
+# start-to-end calculation (and write json file with parameters)
+#
+if True:
+    calculator = Shadow3Calculator("test", None, parameters=get_calculator_parameters())
+    calculator.backengine()
+
+
+    do_plotxy_plot(calculator, title="FULL SIMULATION. ")
+
+    #
+    # save parameters
+    calculator.parameters.to_json("my_parameters.json")
+    print("calculator parameters saved to file: my_parameters.json")
+    # print(calculator.parameters)
+
+
+#
+# calculation retrieving parameters from json file previously created
 #
 if True:
 
-    new_calculator = Shadow3Calculator("from file")
-    new_calculator.setParams(number_of_optical_elements=1, json="my_parameters.json")
+    params_json = CalculatorParameters.from_json("my_parameters.json")
+    new_calculator = Shadow3Calculator("from file", None, parameters=params_json)
+    new_calculator.backengine()
 
-    new_calculator.backengine(write_start_files_root="start_new")
+    # plot
+    do_plotxy_plot(new_calculator, title="PARAMETERS FROM JSON FILE. ")
+
+
+#
+# calculation in two steps:
+#     1) calculate only sources and save to file
+#     2) trace optical element reading source from the file
+
+if True:
+
+    from shadow3libpyvinyl.Shadow3Data import Shadow3Data, Shadow3BeamFormat, Shadow3OpenPMDFormat
+
+    calculator = Shadow3Calculator("test", None, parameters=get_calculator_parameters(source=True,oe=False))
+    calculator.backengine()
+    # calculator.data.write("tmp11.dat", Shadow3BeamFormat)    # raw data format
+    calculator.data.write("tmp11.h5", Shadow3OpenPMDFormat)  # openPMD data format
+
+    # load input data
+    input_data = Shadow3Data("")
+    # input_data.set_file("tmp11.dat", Shadow3BeamFormat)   # raw data format
+    input_data.set_file("tmp11.h5", Shadow3OpenPMDFormat)  # openPMD data format
+
+    # trace oe with saved source
+    new_calculator = Shadow3Calculator("source from file", input_data, parameters=get_calculator_parameters(source=False,oe=True))
+    new_calculator.backengine()
+
     #
-    # new_calculator.saveH5("tmp.h5")
+    # plot
     #
-    import Shadow
-    Shadow.ShadowTools.plotxy(new_calculator.data, 1, 3, nbins=101, nolost=1, title="Real space")
-
-
+    do_plotxy_plot(new_calculator, title="SOURCE FROM FILE. ")
--- shadow3libpyvinyl/Shadow3Data.py
+++ shadow3libpyvinyl/Shadow3Data.py
@@ -1,14 +1,13 @@
+
+
+# treat the Shadow.Beam() data as libpyvinyl data. Two file formats: raw format (original shadow3) and openPMD format.
+
 from libpyvinyl.BaseData import BaseData
 from libpyvinyl.BaseFormat import BaseFormat
-import inspect
-import numpy
-
-import h5py
-import numpy as np
 
 
-from libpyvinyl.BaseData import DataCollection
 import Shadow
+from shadow3libpyvinyl.openPMD import loadShadowOpenPMD, saveShadowToHDF
 
 class Shadow3Data(BaseData):
     def __init__(self,
@@ -18,15 +17,10 @@ class Shadow3Data(BaseData):
                  file_format_class=None,
                  file_format_kwargs=None):
 
-        #  There are only two sections need to consider by the simulation package developers:
-
-        # A dictionary whose keys are the expected keys of the dictionary returned by get_data(), we just simply would like to get a “number” from a NumberData
         expected_data = {}
-        ### DataClass developer's job start
-        # expected_data["number"] = None
-        expected_data["DUMMY"] = None
+        expected_data["nrays"] = None
+        expected_data["rays"] = None
 
-        ### DataClass developer's job end
 
         super().__init__(key,
                          expected_data,
@@ -39,19 +33,20 @@ class Shadow3Data(BaseData):
     def supported_formats(self):
         format_dict = {}
         ### DataClass developer's job start
-        self._add_ioformat(format_dict, Shadow3GfileFormat)
+        self._add_ioformat(format_dict, Shadow3BeamFormat)
+        self._add_ioformat(format_dict, Shadow3OpenPMDFormat)
         ### DataClass developer's job end
         return format_dict
 
 
-class Shadow3GfileFormat(BaseFormat):
+class Shadow3BeamFormat(BaseFormat):
     def __init__(self) -> None:
         super().__init__()
 
     @classmethod
     def format_register(self):
-        key = "Gfile"
-        desciption = "Gfile format for Shadow3Data"
+        key = "shadow3beam_raw"
+        desciption = "Beam format (raw) for Shadow3Beam"
         file_extension = ".dat"
         read_kwargs = [""]
         write_kwargs = [""]
@@ -63,217 +58,73 @@ class Shadow3GfileFormat(BaseFormat):
     def read(cls, filename: str) -> dict:
         """Read the data from the file with the `filename` to a dictionary. The dictionary will
         be used by its corresponding data class."""
-        flag = cls.shadow3_file_type(filename)
-        if flag == 0:
-            oe0 = Shadow.Source()
-            oe0.load(filename)
-            return oe0.to_dictionary()
-        elif flag == 1:
-            oe1 = Shadow.OE()
-            oe1.load(filename)
-            return oe1.to_dictionary()
-        else:
-            raise Exception("Error loading file %s (flag=%d)" % (filename, flag) )
+        print(">>>>>>>>>>>>>>>>>>>>>>>>> reading RAW....")
+        try:
+            beam = Shadow.Beam()
+            beam.load(filename)
+            return {"nrays": beam.nrays(), "rays": beam.rays}
+        except:
+            raise Exception("Error loading file %s" % (filename) )
 
     @classmethod
     def write(cls, object: Shadow3Data, filename: str, key: str = None):
+        print(">>>>>>>>>>>>>>>>>>>>>>>>> writing RAW")
+        b = Shadow.Beam(N=object.get_data()["nrays"])
+        b.rays = object.get_data()["rays"]
+        b.write(filename)
+        print("File %s with shadow3 beam (raw format) written to disk." % filename)
 
-
-        dict1 = object.get_data()
-        keys = dict1.keys()
-        if 'F_WIGGLER' in keys:
-            shadow_object = Shadow.Source()
-            print(">>>>>>>>>>>> source")
-        elif 'F_GRATING' in keys:
-            shadow_object = Shadow.OE()
-            print(">>>>>>>>>>>> oe")
-        else:
-            raise Exception("Mismatch data")
-
-        i_list = cls.__get_valiable_list(shadow_object)
-
-        for name in i_list:
-            try:
-                value = dict1[name]
-                if isinstance(value, str):
-                    value = bytes(value, 'UTF-8')
-                elif isinstance(value, numpy.ndarray):
-                    for list_item in value:
-                        if isinstance(list_item, str):
-                            list_item = bytes(list_item, 'UTF-8')
-                setattr(shadow_object, name, value)
-            except:
-                raise Exception("Error setting parameters name %s" % name)
-
-        shadow_object.write(filename)
-
-        if key is None:
-            original_key = object.key
-            key = original_key + "_to_Shadow3GfileFormat"
-            return object.from_file(filename, cls, key)
-        else:
-            return object.from_file(filename, cls, key)
-
-        # """Save the data with the `filename`."""
-        # data_dict = object.get_data()
-        # number = data_dict["number"]
-        # with h5py.File(filename, "w") as h5:
-        #     h5["number"] = number
-        # if key is None:
-        #     original_key = object.key
-        #     key = original_key + "_to_H5Format"
-        #     return object.from_file(filename, cls, key)
-        # else:
-        #     return object.from_file(filename, cls, key)
 
     @staticmethod
     def direct_convert_formats():
+        # TO DO...
         # Assume the format can be converted directly to the formats supported by these classes:
         # AFormat, BFormat
         # Redefine this `direct_convert_formats` for a concrete format class
         return []
 
-    @staticmethod
-    def shadow3_file_type(filename):
-        with open(filename) as f:
-            if 'F_WIGGLER' in f.read():
-                return 0
-        with open(filename) as f:
-            if 'F_GRATING' in f.read():
-                return 1
-        return -1
+class Shadow3OpenPMDFormat(BaseFormat):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    def format_register(self):
+        key = "shadow3beam_openPMD"
+        desciption = "Beam format (openPMD) for Shadow3Beam"
+        file_extension = ".h5"
+        read_kwargs = [""]
+        write_kwargs = [""]
+        return self._create_format_register(
+            key, desciption, file_extension, read_kwargs, write_kwargs
+        )
+
+    @classmethod
+    def read(cls, filename: str) -> dict:
+        """Read the data from the file with the `filename` to a dictionary. The dictionary will
+        be used by its corresponding data class."""
+        print(">>>>>>>>>>>>>>>>>>>>>>>>> reading openPMD....")
+        try:
+            beam = loadShadowOpenPMD(filename)
+            return {"nrays": beam.nrays(), "rays": beam.rays}
+        except:
+            raise Exception("Error loading file %s" % (filename) )
+
+    @classmethod
+    def write(cls, object: Shadow3Data, filename: str, key: str = None):
+        print(">>>>>>>>>>>>>>>>>>>>>>>>> writing openPMD")
+        b = Shadow.Beam(N=object.get_data()["nrays"])
+        b.rays = object.get_data()["rays"]
+        saveShadowToHDF(b, filename=filename, workspace_units_to_cm=100.0)
+        print("File %s with shadow3 beam (openPMD format) written to disk." % filename)
+
 
     @staticmethod
-    def __get_valiable_list(object1):
-        """
-        returns a list of the Shadow.Source or Shadow.OE variables
-        """
-        mem = inspect.getmembers(object1)
-        mylist = []
-        for i,var in enumerate(mem):
-            if var[0].isupper():
-                mylist.append(var[0])
-        return(mylist)
-
-
-# class TXTFormat(BaseFormat):
-#     def __init__(self) -> None:
-#         super().__init__()
-#
-#     @classmethod
-#     def format_register(self):
-#         key = "TXT"
-#         desciption = "TXT format for NumberData"
-#         file_extension = ".txt"
-#         read_kwargs = [""]
-#         write_kwargs = [""]
-#         return self._create_format_register(
-#             key, desciption, file_extension, read_kwargs, write_kwargs
-#         )
-#
-#     @classmethod
-#     def read(cls, filename: str) -> dict:
-#         """Read the data from the file with the `filename` to a dictionary. The dictionary will
-#         be used by its corresponding data class."""
-#         number = float(np.loadtxt(filename))
-#         data_dict = {"number": number}
-#         return data_dict
-#
-#     @classmethod
-#     def write(cls, object: Shadow3Data, filename: str, key: str = None):
-#         """Save the data with the `filename`."""
-#         data_dict = object.get_data()
-#         arr = np.array([data_dict["number"]])
-#         np.savetxt(filename, arr, fmt="%.3f")
-#         if key is None:
-#             original_key = object.key
-#             key = original_key + "_to_TXTFormat"
-#             return object.from_file(filename, cls, key)
-#         else:
-#             return object.from_file(filename, cls, key)
-#
-#     @staticmethod
-#     def direct_convert_formats():
-#         # Assume the format can be converted directly to the formats supported by these classes:
-#         # AFormat, BFormat
-#         # Redefine this `direct_convert_formats` for a concrete format class
-#         return [H5Format]
-#
-#     @classmethod
-#     def convert(
-#         cls, obj: Shadow3Data, output: str, output_format_class: str, key=None, **kwargs
-#     ):
-#         """Direct convert method, if the default converting would be too slow or not suitable for the output_format"""
-#         if output_format_class is H5Format:
-#             cls.convert_to_H5Format(obj.filename, output)
-#         else:
-#             raise TypeError(
-#                 "Direct converting to format {} is not supported".format(
-#                     output_format_class
-#                 )
-#             )
-#         # Set the key of the returned object
-#         if key is None:
-#             original_key = obj.key
-#             key = original_key + "_from_TXTFormat"
-#             return obj.from_file(output, output_format_class, key)
-#         else:
-#             return obj.from_file(output, output_format_class, key)
-#
-#     @classmethod
-#     def convert_to_H5Format(cls, input: str, output: str):
-#         """The engine of convert method."""
-#         print("Directly converting TXTFormat to H5Format")
-#         number = float(np.loadtxt(input))
-#         with h5py.File(output, "w") as h5:
-#             h5["number"] = number
-#
-#
-# class H5Format(BaseFormat):
-#     def __init__(self) -> None:
-#         super().__init__()
-#
-#     @classmethod
-#     def format_register(self):
-#         key = "H5"
-#         desciption = "H5 format for NumberData"
-#         file_extension = ".h5"
-#         read_kwargs = [""]
-#         write_kwargs = [""]
-#         return self._create_format_register(
-#             key, desciption, file_extension, read_kwargs, write_kwargs
-#         )
-#
-#     @classmethod
-#     def read(cls, filename: str) -> dict:
-#         """Read the data from the file with the `filename` to a dictionary. The dictionary will
-#         be used by its corresponding data class."""
-#         with h5py.File(filename, "r") as h5:
-#             number = h5["number"][()]
-#         data_dict = {"number": number}
-#         return data_dict
-#
-#     @classmethod
-#     def write(cls, object: Shadow3Data, filename: str, key: str = None):
-#         """Save the data with the `filename`."""
-#         data_dict = object.get_data()
-#         number = data_dict["number"]
-#         with h5py.File(filename, "w") as h5:
-#             h5["number"] = number
-#         if key is None:
-#             original_key = object.key
-#             key = original_key + "_to_H5Format"
-#             return object.from_file(filename, cls, key)
-#         else:
-#             return object.from_file(filename, cls, key)
-#
-#     @staticmethod
-#     def direct_convert_formats():
-#         # Assume the format can be converted directly to the formats supported by these classes:
-#         # AFormat, BFormat
-#         # Redefine this `direct_convert_formats` for a concrete format class
-#         return []
-
+    def direct_convert_formats():
+        # TODO...
+        # Assume the format can be converted directly to the formats supported by these classes:
+        # AFormat, BFormat
+        # Redefine this `direct_convert_formats` for a concrete format class
+        return []
 if __name__ == "__main__":
 
 
@@ -283,74 +134,42 @@ if __name__ == "__main__":
     print(data.expected_data)
 
     #
-    # # see: libpyvinyl/tests/unit/test_BaseData.py
+    # add test data
     #
-    # test_data = Shadow3Data(key="test_data")
-    # my_dict = {"number": 4}
-    # test_data.set_dict(my_dict)
-    # print(test_data.get_data())
-    # assert test_data.get_data()["number"] == 4
-    #
-    #
-    # # """Test creating a DataCollection instance with two datasets"""
+    b = Shadow.Beam()
+    b.genSource( Shadow.Source() )
+    data.set_dict( {"nrays":b.nrays(), "rays":b.rays})
 
-    #
-    # test_data1 = Shadow3Data(key="test_data1")
-    # test_data2 = Shadow3Data(key="test_data2")
-    #
-    # test_data1.set_dict({"number": 5})
-    # test_data2.set_dict({"number": 10})
-    #
-    # collection = DataCollection(test_data1, test_data2)
-    # assert collection["test_data1"].get_data()["number"] == 5
-    # assert collection["test_data2"].get_data()["number"] == 10
-    #
-    # value_collection = collection.get_data()
-    # assert value_collection["test_data1"]["number"] == 5
-    # assert value_collection["test_data2"]["number"] == 10
+    # print(data.get_data())
 
-    # Shadow data
-
-
-    oe0 = Shadow.Source()
-    oe0_dict = oe0.to_dictionary()
-
-    shadow_data0 = Shadow3Data(key="oe0")
-    shadow_data0.set_dict(oe0_dict)
-    # print(shadow_data0.get_data())
-
-    oe1 = Shadow.OE()
-    oe1_dict = oe1.to_dictionary()
-    # print(oe0_dict)
-
-    shadow_data1 = Shadow3Data(key="oe1")
-    shadow_data1.set_dict(oe1_dict)
-    # print(shadow_data.get_data())
-
-    print(shadow_data0.get_data())
-    print(shadow_data1.get_data())
-
-    shadow_beamline = DataCollection(shadow_data0, shadow_data1)
-    print(shadow_beamline["oe0"].get_data())
-    print(shadow_beamline["oe1"].get_data())
-
-    print(len(shadow_beamline))
-    print(shadow_beamline.to_list())
 
     # file i/o
-    Shadow3Data.list_formats()
-    oe0 = Shadow.Source()
-    oe0.write("tmp0.dat")
-    test_data = Shadow3Data(key="test_data")
-    test_data.set_file("tmp0.dat", Shadow3GfileFormat)
-    # print(test_data.get_data())
 
-    oe1 = Shadow.OE()
-    oe1.write("tmp1.dat")
-    test_data = Shadow3Data(key="test_data")
-    test_data.set_file("tmp1.dat", Shadow3GfileFormat)
-    test_data.write('tmp11.dat', Shadow3GfileFormat)
-    tmp = Shadow.OE()
-    tmp.load('tmp11.dat')
-    # print(test_data.get_data())
+    Shadow3Data.list_formats()
+
+    # write RAW
+    data.write('tmp11.dat', Shadow3BeamFormat)
+
+    # read RAW
+    loaded_data = Shadow3Data(key="test_data",
+                            data_dict=None,
+                            filename=None,
+                            file_format_class=None,
+                            file_format_kwargs=None
+                            )
+    loaded_data.set_file("tmp11.dat", Shadow3BeamFormat)
+    print(loaded_data.get_data()["nrays"], loaded_data.get_data()["rays"].shape)
+
+    # write OpenPMD
+    data.write('tmp11.h5', Shadow3OpenPMDFormat)
+
+    # read OpenPMD
+    loaded_data = Shadow3Data(key="test_data",
+                            data_dict=None,
+                            filename=None,
+                            file_format_class=None,
+                            file_format_kwargs=None
+                            )
+    loaded_data.set_file("tmp11.h5", Shadow3OpenPMDFormat)
+    print(loaded_data.get_data()["nrays"], loaded_data.get_data()["rays"].shape)
 
